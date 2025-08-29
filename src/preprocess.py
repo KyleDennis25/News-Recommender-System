af2c9967c78525f2bde58cc2f2b9c375859facb9
Kyle Dennis <kyled.525@icloud.com>
2025-08-28 20:02:29 -0400

    Update preprocess.py
    Documentation

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,4 +1,3 @@
-import os
 import re
 import pandas as pd
 import string
@@ -6,7 +5,8 @@ import string
 
 def clean_text(text):
     """
-    Cleans a string- lowercases string and removes trivial characters and stopwords .
+    Cleans a text string (lowercases text, removes trivial characters, and tokenizes text).
+    Returns cleaned string.
     """
     text = text.lower()
     # Removes standard punctuation and quotes
@@ -17,23 +17,22 @@ def clean_text(text):
     tokens = text.split()
     return " ".join(tokens)
 
+
 def preprocess(df):
     """
-    Converts list of NewsAPI articles into a pandas DataFrame.
+    Preprocesses article DataFrame (combines title and text columns, and cleans text).
+    Returns cleaned DataFrame with relevant columns.
     """
-    # Drops nulls
-    df = df.dropna()
+    df = df.dropna().copy()
     # Combine title and description into text column
     df["text"] = df["title"] + " " + df["description"]
     # Clean text
     df["text"] = df["text"].apply(clean_text)
-    # Keep relevant columns
     return df[["title", "topic", "text", "source", "publishedAt", "url"]]
 
+
 if __name__ == "__main__":
-    # preprecesses raw data and saves to data directory
+    # Preprocesses raw data and saves to data directory
     df_raw = pd.read_csv("data/article_data.csv")
     df_clean = preprocess(df_raw)
     df_clean.to_csv("data/clean_article_data.csv", index=False)
-    
-    
